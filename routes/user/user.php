a61e9033c52df6663e78d3318d3aa2dbef4d6ade
anton_maliar <AntonMaliar0@gmail.com>
2023-10-25 19:44:06 +0300

    changed url for 'profile' route and added new 'edit-profile' route

--- routes/user/user.php
+++ routes/user/user.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/profile/{id}', function(){
+Route::get('/profile', function(){
     $user = auth()->user();
     return view('profile', ['user' => $user]);
 })->middleware('auth');
+
+Route::get('/edit-profile', function(){
+    $user = auth()->user();
+    return view('edit-profile', ['user' => $user]);
+});
+
+Route::put('/edit-profile', [UserController::class, 'edit']);
