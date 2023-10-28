4569711684bcaad1a4657898d50ae44d13fad6b3
anton_maliar <AntonMaliar0@gmail.com>
2023-10-28 15:43:53 +0300

    create one to many relationship with SubTask

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,8 +3,8 @@
 namespace App\Models;
 
 use App\Models\Util\Status;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Task extends Model
 {
@@ -12,4 +12,9 @@ class Task extends Model
     protected $attributes = [
         'status' => Status::INPROGRESS
     ];
+
+    public function subTasks(): HasMany
+    {
+        return $this->hasMany(SubTask::class);
+    }
 }
