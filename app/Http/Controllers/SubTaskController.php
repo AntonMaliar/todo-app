5b2482f949013b159067880f8e043322d458c575
anton_maliar <AntonMaliar0@gmail.com>
2023-10-28 15:58:01 +0300

    create function to save sub tasks in DB

--- app/Http/Controllers/SubTaskController.php
+++ app/Http/Controllers/SubTaskController.php
@@ -3,9 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\SubTask;
 use Illuminate\Http\Request;
 
 class SubTaskController extends Controller
 {
-    //
+    public function addSubTask($taskId ,Request $request) {
+        $subTasks = $request->input('sub-tasks');
+
+        foreach($subTasks as $st) {
+            $st->task_id = $taskId;
+        }
+        //add task id for each sub task
+        //send batch of task to db
+        SubTask::createMany($subTasks);
+    }
 }
